acec22e06cf68b03ff0b744e32b031906f6e62b8
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-05-30 14:05:52 +0700

    Modify es6

--- js-data-structure/es6/getter-setter.js
+++ js-data-structure/es6/getter-setter.js
@@ -1,16 +1,20 @@
 // Only change code below this line
 class Thermostat{
+  constructor(temp) {
+    this._temp = 5/9*(temp - 32);
 
-  constructor(temp){
-    this._temp = temp;
   }
+  
+  // Getter 
   get temperature(){
-    return this._temp
+    return this._temp;
+  }
+  // Setter
+  set temperature(updateTemp){
+    this._temp =  updateTemp; 
+  }
 
-  }
-  set temperature(cel){
-    this._temp =  5/9 * (this._temp - cel)     
-  }
+  
 }
 // Only change code above this line
 
@@ -18,4 +22,3 @@ const thermos = new Thermostat(76); // Setting in Fahrenheit scale
 let temp = thermos.temperature; // 24.44 in Celsius
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
-console.log(temp)
