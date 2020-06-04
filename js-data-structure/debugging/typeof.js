4f18a5662a2abf6f4800c46bfecbde1ee7ac2696
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-06-04 16:52:27 +0700

    complete debugging

---
+++ js-data-structure/debugging/typeof.js
@@ -0,0 +1,12 @@
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven)
+console.log(typeof three)
+
+//MOST COMMMON ERROR//
+/*
+Almost every value on its own in JavaScript evaluates to true,
+except what are known as the "falsy" values: false, 0, "" (an empty string), NaN, undefined, and null
+*/
